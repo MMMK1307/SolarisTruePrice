73466b1781587f1993767d4e2dbdf38cd6c37d9e
MMMK1307 <mikael.strapasson1@gmail.com>
2023-04-07 20:53:11 -0300

    Price Calculations and Adaptations

--- contentScript.js
+++ contentScript.js
@@ -3,22 +3,42 @@
     let currentFigureId = "";
     let figureTruePrice = "";
     let figurePriceDifference = "";
-    let ShippingMethods = ['saver', 'regular', 'express'];
+    let currencyTemp = 0;
+    const ShippingMethods = ['saver', 'regular', 'express'];
+    const currencyPatern1 = {
+        decimal: ',',
+        hundred: '.',
+        id: 0
+    };
+    const currencyPatern2 = {
+        decimal: '.',
+        hundred: ',',
+        id: 1
+    };
+    const currencySignsPatern = [currencyPatern1, currencyPatern2];
+    let currencySignsPaternId = 0;
+
+    const currency = {
+        symbol: ['¥','$','€','£','SFr.','$','$','R$'],
+        country: ['JPY','USD','EUR','GBP','CHF','CAD','AUD','BRL']
+    }
+
     let preferences = {
         method: 1,
         shippinCost: 150.0,
         porcentage: 0.7,
-        currency: 'BR'
+        currencyId: 7
     }
 
     const showPriceProducts = () => {
 
-        const truePricebtnExists = document.getElementById("truePriceBtn")
+        const truePricebtnExists = document.getElementById("truePriceBtn");
+        observer.disconnect();
 
         if (!truePricebtnExists) {
 
             const truePrieceInfo = document.createElement("div");
-            truePrieceInfo.className = "product-submit__btn product-submit__btn--red"
+            truePrieceInfo.className = "product-submit__btn product-submit__btn--red";
 
             const truePriceButton = document.createElement("button");
             truePriceButton.className = "btn btn__main-content add-js active:!tw-scale-90";
@@ -34,19 +54,42 @@
                 + '<div class="product__btn-desc">price with taxes and shipping</div></div>'
                 + '<div class="h5--body product__price" data-price-wrapper="">'
                 + '<span style="color: #f2ccf2; font-size: 12px; position: relative; right: 7%;"'
-                + 'data-currency-brl="R$ ' + figurePriceDifference + '" data-currency="BRL">R$ ' + figurePriceDifference + '</span>'
+                + 'data-currency-brl="'+ currency.symbol[currencyTemp] +' ' + figurePriceDifference 
+                + '" data-currency="'+ currency.country[currencyTemp] +'">'+ currency.symbol[currencyTemp] +' ' + figurePriceDifference + '</span>'
                 + '<span class="money"'
-                + 'data-currency-brl="R$ ' + figureTruePrice + '" data-currency="BRL">R$ ' + figureTruePrice + '</span></div>'
+                + 'data-currency-brl="'+ currency.symbol[currencyTemp] +' ' + figureTruePrice
+                + '" data-currency="'+ currency.country[currencyTemp] 
+                +'">'+ currency.symbol[currencyTemp] +' ' + figureTruePrice + '</span></div>';
 
             truePriceButton.innerHTML = btnText.innerHTML;
             truePrieceInfo.appendChild(truePriceButton);
 
             priceButton = document.getElementsByClassName("product__form")[0];
             priceButton.appendChild(truePrieceInfo);
+        } else {
+            let newTruePriceButton = document.getElementById('truePriceBtn');
+            calculatePrice("h5--body product__price");
+
+            const btnText = document.createElement("div");
+            btnText.innerHTML = '<div class="product__sale-badge tw-animate-beat"><span class="sale-badge__percent">'
+                + 'UNSALE ' + preferences.porcentage * 100 + '% ON</span>'
+                + '</div><div class="product__btn-text"><div class="product__btn-label">Add to WishList</div>'
+                + '<div class="product__btn-desc">price with taxes and shipping</div></div>'
+                + '<div class="h5--body product__price" data-price-wrapper="">'
+                + '<span style="color: #f2ccf2; font-size: 12px; position: relative; right: 7%;"'
+                + 'data-currency-brl="'+ currency.symbol[currencyTemp] +' ' + figurePriceDifference 
+                + '" data-currency="'+ currency.country[currencyTemp] +'">'+ currency.symbol[currencyTemp] +' ' 
+                + figurePriceDifference + '</span>'
+                + '<span class="money"'
+                + 'data-currency-brl="'+ currency.symbol[currencyTemp] +' ' + figureTruePrice + '" data-currency="'+ currency.country[currencyTemp] 
+                +'">'+ currency.symbol[currencyTemp] +' ' + figureTruePrice + '</span></div>';
+
+            newTruePriceButton.innerHTML = btnText.innerHTML;
         }
+        startObserving();
     }
 
-    const  showPriceProductsList = () => {
+    const showPriceProductsList = () => {
         const productList = document.getElementsByClassName('product__labels-wrapper');
         observer.disconnect();
 
@@ -54,13 +97,13 @@
 
             if (productList[i].innerText.includes('TRUE PRICE'))
                 continue;
-            
+
             let listedPrice = productList[i].getElementsByClassName('money')[0];
 
             if (listedPrice == null)
                 continue;
-            
-            if (!listedPrice.innerText.includes('R$'))
+
+            if (!listedPrice.innerText.includes(currency.symbol[currencyTemp]))
                 continue;
 
             calculatePriceList(listedPrice);
@@ -70,7 +113,7 @@
             buttonDiv.style = "background-color: rgb(153, 24, 153);";
             buttonDiv.innerHTML = '<span class="product-label__title">True Price</span>'
                 + '<span class="price">'
-                + '<span class="money" data-currency="BRL">R$ ' + figureTruePrice + '</span>'
+                + '<span class="money" data-currency="'+ currency.country[currencyTemp] +'">'+ currency.symbol[currencyTemp] +' ' + figureTruePrice + '</span>'
                 + '</span>';
 
             const buttonSpace = productList[i];
@@ -80,23 +123,22 @@
     }
 
     const showPriceProductsSearch = () => {
-        waitForElementToLoad('.product__labels-wrapper .money', 1000, 200000 );
+        //waitForElementToLoad('.product__labels-wrapper .money', 1000, 200000);
         const productList = document.getElementsByClassName('product__labels-wrapper');
         observer.disconnect();
 
         for (let i = 0; i < productList.length; i++) {
-        
-            if (productList[i].innerText.includes('TRUE PRICE'))
-            {
-                if(productList[i].innerText.includes('not Available')){
+
+            if (productList[i].innerText.includes('TRUE PRICE')) {
+                if (productList[i].innerText.includes('not Available')) {
                     const lent = (productList.length) - 1;
                     productList[lent].remove();
                 } else {
                     continue;
                 }
             }
-            
-            waitForElementToLoad('iut', 1000, 20000 );
+
+            //waitForElementToLoad('iut', 1000, 20000);
 
             let listedPrice = productList[i].getElementsByClassName('money')[0];
 
@@ -104,18 +146,15 @@
                 figureTruePrice = "0.00 not Available";
                 continue;
             } else {
-                console.log(listedPrice.innerText);
                 calculatePriceList(listedPrice);
             }
-
-            console.log(figureTruePrice);
 
             const buttonDiv = document.createElement('div');
             buttonDiv.className = 'product-label product-label--pre-order tw-relative';
             buttonDiv.style = "background-color: rgb(153, 24, 153);";
             buttonDiv.innerHTML = '<span class="product-label__title">True Price</span>'
                 + '<span class="price">'
-                + '<span class="money" data-currency="BRL">R$ ' + figureTruePrice + '</span>'
+                + '<span class="money" data-currency="'+ currency.country[currencyTemp] +'">'+ currency.symbol[currencyTemp] +' ' + figureTruePrice + '</span>'
                 + '</span>';
 
             const buttonSpace = productList[i];
@@ -125,7 +164,7 @@
     }
 
     const addPorcetage = (cleanPrice) => {
-        cleanPrice = cleanPrice.split("R$");
+        cleanPrice = cleanPrice.split(currency.symbol[currencyTemp]);
         cleanPrice = formatStringToFloat(cleanPrice[cleanPrice.length - 1]);
         calcPrice = ((cleanPrice + calcShipping) + ((cleanPrice + calcShipping) * preferences.porcentage)).toFixed(2);
 
@@ -140,6 +179,7 @@
             figureTruePrice = "0.00 not Available";
             return;
         }
+
         calcShipping = getShippingPrice();
         addPorcetage(allListedPrices[0].innerText);
     }
@@ -150,24 +190,60 @@
     }
 
     const formatNumberToString = (ogNumber) => {
-        ogNumber = ogNumber.toString().replace('.', ',');
 
+        if(currencySignsPaternId == currencyPatern1.Id){
+            ogNumber = ogNumber.toString().replace('.', ',');
+        }
+        
         if (ogNumber.length > 6) {
             pointposition = ogNumber.length - 6;
-            ogNumber = [ogNumber.slice(0, pointposition), '.', ogNumber.slice(pointposition)].join('');
+            ogNumber = [ogNumber.slice(0, pointposition), currencySignsPatern[currencySignsPaternId].hundred, ogNumber.slice(pointposition)].join('');
         }
-        return ogNumber
+        return ogNumber;
     }
 
     const formatStringToFloat = (ogString) => {
-        return parseFloat(ogString.replace('.', '').replace(',', '.'));
+        if(currencySignsPaternId == currencyPatern1.id){
+            return parseFloat(ogString.replace('.', '').replace(',', '.'));
+        }
+        return parseFloat(ogString);
+    }
+
+    const getCurrencyPatern = () => {
+        if (currencyTemp == 0 || currencyTemp == 7){
+            currencySignsPaternId = currencyPatern1.id;
+            return;
+        }
+        currencySignsPaternId = currencyPatern2.id;
+    } 
+
+    const updateCurrency = () => {
+        let currencyExample = document.getElementsByClassName('money')[0].innerText.split(' ')[0];
+        
+        for (i = 0; i < currencyExample.length; i++){
+           if(!isNaN(currencyExample.charAt(i))){
+                currencyExample =  currencyExample.substring(0, i);
+           }
+        }
+        
+        for(i = 0; i < currency.symbol.length; i++){
+            if (currencyExample == currency.symbol[i]){
+                currencyTemp = i;
+                return;
+            }
+        }
+
+        console.log("Default Currency");
+        currencyTemp = preferences.currencyId;
     }
 
     const receivePageLoad = (type, figureId) => {
+        console.log(type);
 
         currentFigureId = figureId;
+        updateCurrency();
+        getCurrencyPatern();
 
-        console.log(type);
         switch (type) {
             case 'home':
                 showPriceProductsList();
@@ -188,17 +264,23 @@
 
     const getShippingPrice = () => {
 
-        waitForElementToLoad(".worldwide-shipping .money", 1000, 200000);
+        //waitForElementToLoad(".worldwide-shipping .money", 1000, 200000);
         let listShippingPrices = document.getElementsByClassName('worldwide-shipping')[0].getElementsByClassName('money');
         let returnPrice;
 
         if (listShippingPrices.length > 0) {
+
+            if (!listShippingPrices[1].innerText.includes(currency.symbol[currencyTemp])) {
+                return preferences.shippinCost;
+            }
+
             if (document.querySelector("#" + ShippingMethods[preferences.method] + " .money")) {
                 returnPrice = listShippingPrices[preferences.method].innerText;
             } else {
                 returnPrice = listShippingPrices[0].innerText;
             }
-            return formatStringToFloat(returnPrice.split("R$")[1]);
+            
+            return formatStringToFloat(returnPrice.split(currency.symbol[currencyTemp])[1]);
         }
         return preferences.shippinCost;
     }
@@ -206,12 +288,14 @@
     const waitForElementToLoad = (selector, checkFrequencyInMs, timeoutInMs) => {
         let startTimeInMs = Date.now();
         (function loopSearch() {
-            if (document.querySelector(selector) != null)
+            if (document.querySelector(selector) != null) {
                 return;
+            }
             else {
                 setTimeout(() => {
-                    if (timeoutInMs && Date.now() - startTimeInMs > timeoutInMs)
+                    if (timeoutInMs && Date.now() - startTimeInMs > timeoutInMs) {
                         return;
+                    }
                     loopSearch();
                 }, checkFrequencyInMs);
             }
@@ -224,14 +308,14 @@
             const linkInfo = url.split("com")[1];
             let productType = linkInfo.split("/")[1];
 
-            if (linkInfo.includes('products'))
+            if (linkInfo.includes('products')) {
                 productType = 'products';
-            
-            if (linkInfo == '/'){
+            }
+
+            if (linkInfo == '/') {
                 receivePageLoad('home', '');
                 return;
             }
-
             receivePageLoad(productType, linkInfo.split("/")[2].split("?")[0]);
         }
     }
@@ -239,11 +323,9 @@
     MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
     let observer = new MutationObserver(function (mutations, observer) {
-        
-        if (mutations.length > 30){
-            showPriceProductsList();
-        } else if (mutations.length > 3) {
-            //showPriceProductsSearch();
+        //console.log(mutations);
+        if (mutations.length > 2) {
+            windowReloaded();
         }
     });
 
@@ -264,4 +346,3 @@
         windowReloaded();
     });
 })();
-
